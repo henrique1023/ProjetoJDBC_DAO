d621a90b4b79c3d042e1bec4d4a76237cc29d601
henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-03-10 11:51:53 -0300

    Project created

---
+++ src/db/DB.java
@@ -0,0 +1,51 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+	
+	private static Connection conn = null;
+	
+	//essa função vai fazer a conexão ao SQL
+	//DriverManager devolve o atributo para conexão do connection
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	//essa função serve para fechar a conexão com o banco
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	//essa função serve para acessar o arquivo do tipo properties e fazer a leitura
+	//precisa estar dentro de um try catch
+	private static Properties loadProperties() {
+		try(FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}catch(IOException e){
+			throw new DbException(e.getMessage());
+		}
+	}
+}
