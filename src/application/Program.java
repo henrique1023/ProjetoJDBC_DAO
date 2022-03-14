b11dd78f4bed2f72f8c86068a9c3cac3763fbf04
henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-03-14 20:56:32 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,9 +13,17 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("==========test seller #1 =============");
 		Seller seller = sellerDao.findById(3);
-		
-		System.out.println(seller);
+			System.out.println(seller);
+			
+		System.out.println("==========test seller #2 =============");
+		Department dep = new Department(1, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+
+		for(Seller l : list) {
+			System.out.println(l);
+		}
 	}
 
 }
