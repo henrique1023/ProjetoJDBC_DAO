e781f1bd86fc210bdb6b6c66ce7acccfc6a0d83f
henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-03-15 11:17:56 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,18 +10,22 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("==========test seller #1 =============");
 		Seller seller = sellerDao.findById(3);
-			System.out.println(seller);
-			
+		System.out.println(seller);
+
 		System.out.println("==========test seller #2 =============");
 		Department dep = new Department(1, null);
 		List<Seller> list = sellerDao.findByDepartment(dep);
-
-		for(Seller l : list) {
+		for (Seller l : list) {
+			System.out.println(l);
+		}
+		System.out.println("==========test seller #3 =============");
+		List<Seller> list2 = sellerDao.findAll();
+		for(Seller l : list2) {
 			System.out.println(l);
 		}
 	}
