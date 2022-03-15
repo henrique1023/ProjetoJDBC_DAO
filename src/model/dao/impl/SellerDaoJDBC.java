08f97f13944cf52f0d262dfd6f43c4d8a8056b13
henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-03-15 16:58:18 -0300

    Project 1/3

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -29,8 +29,7 @@ public class SellerDaoJDBC implements SellerDao {
 		PreparedStatement st = null;
 
 		try {
-			st = conn.prepareStatement("INSERT INTO seller " + 
-					"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
@@ -63,10 +62,8 @@ public class SellerDaoJDBC implements SellerDao {
 		PreparedStatement st = null;
 
 		try {
-			st = conn.prepareStatement(
-					"UPDATE seller " +
-					"SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "+
-					"WHERE Id = ?");
+			st = conn.prepareStatement("UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " + "WHERE Id = ?");
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
@@ -88,13 +85,10 @@ public class SellerDaoJDBC implements SellerDao {
 		PreparedStatement st = null;
 
 		try {
-			st = conn.prepareStatement(
-					"DELETE FROM seller "+
-					"WHERE Id = ?");
+			st = conn.prepareStatement("DELETE FROM seller " + "WHERE Id = ?");
 			st.setInt(1, id);
 
 			st.executeUpdate();
-
 
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
